e3e30ae04d7604cc796b5e4e72ad6d40c07aecb5
dirty_hippy <piaiws@gmail.com>
2021-11-05 18:32:57 +0300

    added bar

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -4,6 +4,7 @@ import os
 from bs4 import BeautifulSoup  # type: ignore
 from urllib.parse import urlparse, urljoin
 import logging
+from progress.bar import Bar  # type: ignore
 
 
 POSTFIX_RESOURCE_PATH = '_files'
@@ -63,7 +64,7 @@ def save_all_resources(soup: BeautifulSoup,
                        url: str,
                        output_path: str,
                        resource_path: str):
-
+    bar = Bar('Processing', max=len(soup.findAll(list(RESOURCES.keys()))))
     full_resource_path = os.path.join(output_path, resource_path)
     try:
         os.mkdir(full_resource_path)
@@ -72,14 +73,15 @@ def save_all_resources(soup: BeautifulSoup,
         logging.error(error_msg)
         raise OSError(error_msg)
     for tag, inner_tag in RESOURCES.items():
-        save_resource(soup, tag, inner_tag, url, full_resource_path)
+        save_resource(soup, tag, inner_tag, url, full_resource_path, bar)
 
 
 def save_resource(soup: BeautifulSoup,
                   tag: str,
                   inner_tag: str,
                   url: str,
-                  resource_path: str):
+                  resource_path: str,
+                  bar: Bar):
 
     for res in soup.findAll(tag):
         res_name = os.path.basename(res[inner_tag])
@@ -91,6 +93,7 @@ def save_resource(soup: BeautifulSoup,
         res_path = os.path.join(resource_path, res_name)
         res[inner_tag] = os.path.join(os.path.basename(resource_path), res_name_new)
         if not os.path.isfile(res_path):
+            response = requests.get(res_url)
             with open(res_path, 'wb') as file:
-                response = requests.get(res_url)
                 file.write(response.content)
+        bar.next()
