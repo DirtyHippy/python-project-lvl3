513ab9747a0c6bd296f301784f38eb5c8e0535f6
dirty_hippy <piaiws@gmail.com>
2021-11-07 22:32:07 +0300

    sysexit

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,18 +1,22 @@
 import argparse
 import os
+import sys
 from page_loader.loader import download
 
 
 def main():
-    parser = argparse.ArgumentParser(description='page loader')
-    parser.add_argument('url', type=str)
-    parser.add_argument('-o',
-                        '--output',
-                        default=os.getcwd(),
-                        help='set output path',
-                        type=str)
-    args = parser.parse_args()
-    download(args.url, args.output)
+    try:
+        parser = argparse.ArgumentParser(description='page loader')
+        parser.add_argument('url', type=str)
+        parser.add_argument('-o',
+                            '--output',
+                            default=os.getcwd(),
+                            help='set output path',
+                            type=str)
+        args = parser.parse_args()
+        download(args.url, args.output)
+    except Exception:
+        sys.exit()
 
 
 if __name__ == '__main__':
