fc6455ba3f06951a82a389072c463bf390fcd4a7
dirty_hippy <piaiws@gmail.com>
2021-11-22 14:33:02 +0300

    added test content

--- page_loader/tests/test_page_loader.py
+++ page_loader/tests/test_page_loader.py
@@ -6,10 +6,37 @@ from page_loader.loader import download, PARSER, POSTFIX_RESOURCE_PATH
 from bs4 import BeautifulSoup  # type: ignore
 from requests_mock import ANY
 from page_loader.exceptions import AppInternalError
+from urllib.parse import urlparse, urljoin
 
 
 SIMPLE_URL = 'http://www.simplehtmlguide.com/examples/images2.html'
 COURCES_URL = 'https://ru.hexlet.io/courses'
+
+SIMPLE_RESOURCES = [
+    (
+        '/examples/photo.jpg',
+        'www-simplehtmlguide-com-examples-photo.jpg'
+    )
+]
+
+COURCES_RESOURCES = [
+    (
+        '/courses',
+        'ru-hexlet-io-courses.html'
+    ),
+    (
+        '/assets/application.css',
+        'ru-hexlet-io-assets-application.css'
+    ),
+    (
+        '/assets/professions/nodejs.png',
+        'ru-hexlet-io-assets-professions-nodejs.png'
+    ),
+    (
+        '/packs/js/runtime.js',
+        'ru-hexlet-io-packs-js-runtime.js'
+    ),
+]
 
 
 def get_fixture_path(file_name):
@@ -17,8 +44,8 @@ def get_fixture_path(file_name):
     return os.path.join(current_dir, 'fixtures', file_name)
 
 
-def read(file_path):
-    with open(file_path, 'r') as f:
+def read(file_path, mode='r'):
+    with open(file_path, mode) as f:
         result = f.read()
     return result
 
@@ -38,11 +65,15 @@ def test_os_error(requests_mock, output_path):
             download(SIMPLE_URL, output_path)
 
 
-@pytest.mark.parametrize("test_file, result_expected, resources_expected, url", [
-    ('simple.html', 'simple_expected.html', 'simple_resources_expected.txt', SIMPLE_URL),
-    ('courses.html', 'courses_expected.html', 'courses_resources_expected.txt', COURCES_URL)])
-def test_download(requests_mock, test_file, result_expected, resources_expected, url):
-    requests_mock.get(ANY, text='any resourse')
+@pytest.mark.parametrize("test_file, result_expected, resources_expected, url, resources", [
+    ('simple.html', 'simple_expected.html', 'simple_resources_expected.txt', SIMPLE_URL, SIMPLE_RESOURCES),         # noqa E501
+    ('courses.html', 'courses_expected.html', 'courses_resources_expected.txt', COURCES_URL, COURCES_RESOURCES)])   # noqa E501
+def test_download(requests_mock, test_file, result_expected, resources_expected, url, resources):
+    netloc = urlparse(url).netloc
+    for resource in resources:
+        resource_path, downloaded_resource = resource
+        requests_mock.get(urljoin(netloc, resource_path), content=read(
+            get_fixture_path(downloaded_resource), 'rb'))
     requests_mock.get(url, text=read(get_fixture_path(test_file)))
     with tempfile.TemporaryDirectory() as tmpdirname:
         file_name = download(url, tmpdirname)
@@ -50,6 +81,10 @@ def test_download(requests_mock, test_file, result_expected, resources_expected,
             read(get_fixture_path(result_expected)), PARSER)
         assert read(file_name) == soup_expected.prettify()
         name, _ = os.path.splitext(file_name)
-        resources = os.listdir(name + POSTFIX_RESOURCE_PATH)
+        resources_list = os.listdir(name + POSTFIX_RESOURCE_PATH)
         resources_expected = read(get_fixture_path(resources_expected)).split()
-        assert sorted(resources) == sorted(resources_expected)
+        assert sorted(resources_list) == sorted(resources_expected)
+        for resource in resources:
+            _, downloaded_resource = resource
+            assert read(get_fixture_path(downloaded_resource), 'rb') == read(
+                os.path.join(name + POSTFIX_RESOURCE_PATH, downloaded_resource), 'rb',)
